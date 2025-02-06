529ed2a6afcf151c2a36a76036a71b061639b91c
blocsu <dotsenko.sar@gmail.com>
2025-02-06 20:57:33 +0400

    first commit

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+// class MyDocument extends Document {
+// 	static async
+// }
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
