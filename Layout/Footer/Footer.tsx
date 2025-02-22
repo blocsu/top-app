e439e0c560411e8f1fb83ed510da95b5bfde25f2
blocsu <dotsenko.sar@gmail.com>
2025-02-22 22:46:39 +0400

    Упражнение - Вёрстка footer

--- Layout/Footer/Footer.tsx
+++ Layout/Footer/Footer.tsx
@@ -1,12 +1,19 @@
+import cn from 'classnames';
+import { format } from 'date-fns';
 import { JSX } from 'react';
+import styles from './Footer.module.css';
 import { FooterProps } from './Footer.props';
 
 
-export const Footer = ({...props}: FooterProps): JSX.Element => {
+export const Footer = ({className, ...props}: FooterProps): JSX.Element => {
 	return (
-		<div {...props} >
-			Footer!!!
-		</div>
+		<footer className={cn(className, styles.footer)} {...props} >
+			<div>
+				OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены
+			</div>
+			<a href='#' target='blank'>Пользовательское соглашение</a>
+			<a href='#' target='blank'>Политика конфиденциальности</a>
+		</footer>
 	)
 	
 };
