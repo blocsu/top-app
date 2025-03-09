800b28785ce49a278c028d7f14c3c10ad78d014b
blocsu <dotsenko.sar@gmail.com>
2025-03-09 09:29:12 +0400

    Component Link

--- Layout/Menu/Menu.tsx
+++ Layout/Menu/Menu.tsx
@@ -2,6 +2,7 @@ import { AppContext } from '@/context/app.context';
 import { FirstLevelMenuItem, PageItem } from '@/interfaces/menu.interface';
 import { TopLevelCategory } from '@/interfaces/page.interface';
 import cn from 'classnames';
+import Link from 'next/link';
 import { JSX, useContext } from 'react';
 import BooksIcon from './icons/books.svg';
 import CoursesIcon from './icons/courses.svg';
@@ -24,14 +25,16 @@ export const Menu = (): JSX.Element => {
 			<>
 				{firstLevelMenu.map(m => (
 					<div key={m.route}>
-						<a href={`/${m.route}`}>
-							<div className={cn(styles.firstLevel, {
-								[styles.firstLevelActive]: m.id == firstCategory
-							})}>
-								{m.icon}
-								<span >{m.name}</span>
-							</div>
-						</a>
+						<Link href={`/${m.route}`} legacyBehavior>						
+							<a href={`/${m.route}`}>
+								<div className={cn(styles.firstLevel, {
+									[styles.firstLevelActive]: m.id == firstCategory
+								})}>
+									{m.icon}
+									<span >{m.name}</span>
+								</div>
+							</a>
+						</Link>
 						{m.id == firstCategory && buildSecondLevel(m)}
 					</div>
 				))}
@@ -59,11 +62,13 @@ export const Menu = (): JSX.Element => {
 	const buildThirdLevel = (pages: PageItem[], route: string) => {
 		return (
 			pages.map(p => (
-				<a href={`/${route}/${p.alias}`} className={cn(styles.thirdLevel, {
-					[styles.thirdLevelActive]: false
-				})}>
-					{p.category}
-				</a>
+				<Link href={`/${route}/${p.alias}`} legacyBehavior>
+					<a className={cn(styles.thirdLevel, {
+						[styles.thirdLevelActive]: false
+					})}>
+						{p.category}
+					</a>
+				</Link>
 			))
 		);
 	};
