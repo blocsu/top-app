780b98f94270c646cac0e6a59606556adeaf499e
blocsu <dotsenko.sar@gmail.com>
2025-03-23 20:12:24 +0400

    Склонение слов

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -1,4 +1,4 @@
-import { priceRu } from '@/helpers/helpers';
+import { declOfNum, priceRu } from '@/helpers/helpers';
 import { JSX } from 'react';
 import { Button } from '../Button/Button';
 import { Card } from '../Card/Card';
@@ -25,10 +25,18 @@ export const Product = ({product, className, ...props}: ProductProps): JSX.Eleme
 			<div className={styles.tags}>{product.categories.map(c => <Tag key={c} className={styles.category} color='ghost'>{c}</Tag>)}</div>
 			<div className={styles.priceTitle}>цена</div>
 			<div className={styles.creditTitle}>кредит</div>
-			<div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
+			<div className={styles.rateTitle}>{product.reviewCount} {declOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}</div>
 			<Divider className={styles.hr} />
 			<div className={styles.description}>{product.description}</div>
-			<div className={styles.feature}>фичи</div>
+			<div className={styles.feature}>
+				{product.characteristics.map(c => (
+					<div className={styles.characteristics} key={c.name}>
+						<span className={styles.characteristicsName}>{c.name}</span>
+						<span className={styles.characteristicsDots}></span>
+						<span className={styles.characteristicsValue}>{c.value}</span>
+					</div>
+				))}
+			</div>
 			<div className={styles.advBlock}>
 				{product.advantages && <div className={styles.advantages}>
 					<div className={styles.advTitle}>Преимущества</div>
