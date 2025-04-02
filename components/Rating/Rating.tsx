bdac57de65a0944fbd74f76d1ed3ae465394b906
blocsu <dotsenko.sar@gmail.com>
2025-04-02 21:12:48 +0400

    Обработка ошибок Rating

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -4,7 +4,7 @@ import styles from './Rating.module.css';
 import { RatingProps } from './Rating.props';
 import StarIcon from './star.svg';
 
-export const Rating = forwardRef(({isEditable = false, rating, setRating, ...props}: RatingProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
+export const Rating = forwardRef(({isEditable = false, error, rating, setRating, ...props}: RatingProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
 	const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>));
 
 	useEffect(() => {
@@ -56,8 +56,11 @@ export const Rating = forwardRef(({isEditable = false, rating, setRating, ...pro
 	}
 
 	return (
-		<div {...props} ref={ref}>
+		<div {...props} ref={ref} className={cn(styles.ratingWrapper, {
+			[styles.error]: error
+		})}>
 			{ratingArray.map((r, i) => (<span key={i}>{r}</span>))}
+			{error && <span className={styles.errorMessage}>{error.message}</span>}
 		</div>
 	);	
 });
