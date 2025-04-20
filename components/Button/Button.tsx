c53dab53304bb5f3b770c4f975a7183407129a05
blocsu <dotsenko.sar@gmail.com>
2025-04-20 12:29:16 +0400

    Жесты и MotionValues

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,12 +1,19 @@
 import cn from 'classnames';
-import { JSX } from 'react';
+import { motion, useMotionValue } from 'framer-motion';
+import { JSX, useEffect } from 'react';
 import ArrowIcon from './arrow.svg';
 import styles from './Button.module.css';
 import { ButtonProps } from './Button.props';
 
 export const Button = ({appearance, arrow = 'none', children, className, ...props}: ButtonProps): JSX.Element => {
+	const scale = useMotionValue(1);
+
+	useEffect(() => {
+		scale.on('change', (s) => {console.log(s)})
+	}, []);
 	return (
-		<button
+		<motion.button
+			whileHover={{scale: 1.05}}
 			className={cn(styles.button, className, {
 				[styles.primary]: appearance == 'primary',
 				[styles.ghost]: appearance == 'ghost'
@@ -19,6 +26,6 @@ export const Button = ({appearance, arrow = 'none', children, className, ...prop
 			})}>
 					<ArrowIcon />
 				</span>}
-		</button>
+		</motion.button>
 	);
 };
