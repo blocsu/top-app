dba4b68102590fae76c1fa4ad6a8cf77ade11b93
blocsu <dotsenko.sar@gmail.com>
2025-05-15 20:33:40 +0400

    Установка метрики

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Router from 'next/router';
 import { JSX } from 'react';
+import ym, { YMInitializer } from 'react-yandex-metrika';
+
+	Router.events.on('routeChangeComplete', (url: string) => {
+	if (typeof window !== 'undefined') {
+		ym('hit', url);
+	}
+});
 
 export default function App({ Component, pageProps, router }: AppProps): JSX.Element {
   return <>
@@ -12,11 +20,17 @@ export default function App({ Component, pageProps, router }: AppProps): JSX.Ele
         <link key={1} rel="icon" href="/favicon_2.ico" />
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
+				<link rel="preconnect" href="https://mc.yandex.ru" />
 				<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
 NEXT_PUBLIC_DOMAIN=https://courses-top.ru
 				<meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
 				<meta property="og:locale" content="ru_RU" />
 		</Head>
+		<YMInitializer 
+			accounts={[]}
+			options={{webvisor: true, defer: true}}
+			version="2"
+		/>
 		<Component {...pageProps} />
-	</>
+	</>;
 }
