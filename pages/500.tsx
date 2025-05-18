0a5b0325903005acf3de59b9f96456f23540999f
blocsu <dotsenko.sar@gmail.com>
2025-05-18 17:49:20 +0400

    Страницы 404, 500

---
+++ pages/500.tsx
@@ -0,0 +1,13 @@
+import { Htag } from '@/components';
+import { withLayout } from '@/layout/Layout';
+import { JSX } from 'react';
+
+function Error500(): JSX.Element {
+	return (
+			<>
+				<Htag tag='h1'>Ошибка 500</Htag>
+			</>    
+	);
+}
+
+export default withLayout(Error500);
